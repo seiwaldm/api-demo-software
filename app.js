b2ecf1bf2427d003de36f9da15ac8e90285c8c56
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-02-16 21:15:50 +0100

    added function with async/await usage

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Wähle den Button aus dem html-file:
 let button = document.querySelector("#randomPost");
+let buttonAsync = document.querySelector("#randomPostAsync");
 
 button.addEventListener("click", getRandomPost);
+buttonAsync.addEventListener("click", getRandomPostAsync);
 
 function getRandomPost() {
   //generiere eine Zufallszahl zwischen 1 und 100
@@ -26,4 +28,22 @@ function getRandomPost() {
       //hänge das neue element unten am body an
       document.querySelector("body").appendChild(element);
     });
+}
+
+async function getRandomPostAsync() {
+  const postNumber = Math.ceil(Math.random() * 100);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postNumber}`
+  );
+  const post = await response.json();
+  console.log(post);
+  //erschaffe ein neues element vom Typ <div>
+  const element = document.createElement("div");
+  //schreibe html-code in das div.element
+  element.innerHTML = `
+       <h3>${post.title}</h3>
+       <p>${post.body}</p>
+       `;
+  //hänge das neue element unten am body an
+  document.querySelector("body").appendChild(element);
 }
